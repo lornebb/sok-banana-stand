0fd22fc70fdcbf20f12a1dd99c12b48e8d9ec8a8
Nick Dalby <nckdlby@gmail.com>
2022-10-10 16:37:12 +0200

    fix: svg view boxes

--- src/icons/svg-bandcamp.tsx
+++ src/icons/svg-bandcamp.tsx
@@ -5,17 +5,15 @@ export const SvgBandcamp = ({ width, height, fill }: SvgTypes) => (
   <svg
     width={width}
     height={height}
-    viewBox="0 0 24 24"
+    viewBox="0 0 30 30"
     className="mr-2 -ml-1 w-4 h-4"
     aria-hidden="true"
     focusable="false"
     data-prefix="fab"
-    data-icon="paypal"
     role="img"
   >
-    <path
-      fill={fill}
-      d="M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm48.2,326.1h-181L207.9,178h181Z"
-    />
+  
+      
+      <path fill={fill} d="M15,3C8.373,3,3,8.373,3,15s5.373,12,12,12s12-5.373,12-12S21.627,3,15,3z M17.333,18.774H8.578l4.089-7.547h8.755L17.333,18.774z" fill-rule="evenodd" clip-rule="evenodd"></path>
   </svg>
 );
