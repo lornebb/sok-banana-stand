81772f650be06e8fbb53fbba85121d648cf86928
Nick Dalby <nckdlby@gmail.com>
2022-11-16 18:34:36 +0000

    HeroImg as an img tag

---
+++ src/components/HeroImg.tsx
@@ -0,0 +1,9 @@
+const HeroImg = () => {
+  return (
+    <header className="flex max-w-lg justify-center mx-auto">
+      <img src="src/media/img/SOKlarge.png" alt="" />
+    </header>
+  )
+}
+
+export default HeroImg
