942796e4c3997af128e5f5974a637cb200cb2d7f
lornebb <51881266+lornebb@users.noreply.github.com>
2022-11-16 18:58:34 +0100

    added image, added hero component. unfinished

--- src/App.tsx
+++ src/App.tsx
@@ -5,47 +5,47 @@ import { SvgSpotify } from "./icons/svg-spotify"
 import { SvgYoutube } from "./icons/svg-youtube"
 import { SvgAppleMusic } from "./icons/svg-appleMusic"
 import { SvgInstagram } from "./icons/svg-instagram"
-import { YearRelease } from "./components/YearRelease"
+import { HeroImage } from "./components/HeroImage"
 
 export const App = () => {
   return (
-    <main className="grid place-content-center bg-yellow-200 min-h-screen">
-      <h1 className="font-sans font-bold text-yellow-500 text-9xl text-center">
-        sok
-      </h1>
-      <div className="flex gap-3 mt-4 grid grid-flow-col grid-rows-6 md:grid-rows-1 lg:grid-rows-1">
-        <SoMeButton
-          href="https://sonofkong.bandcamp.com/"
-          title="BandCamp"
-        >
-          <SvgBandcamp/>
-        </SoMeButton>
-        <SoMeButton
-          href="https://open.spotify.com/artist/14tfZFUIaavTfIOyqBP9Mp"
-          title="Spotify"
-        >
-          <SvgSpotify/>
-        </SoMeButton>
-        <SoMeButton
-          href="https://www.youtube.com/channel/UCY5pevqMn98aQClj2TBWkoQ"
-          title="Youtube"
-        >
-          <SvgYoutube/>
-        </SoMeButton>
-        <SoMeButton
-          href="https://music.apple.com/artist/son-of-kong/1049109342"
-          title="Apple Music"
-        >
-          <SvgAppleMusic
-          />
-        </SoMeButton>
-        <SoMeButton
-          href="https://instagram.com/sonofkong_?igshid=YmMyMTA2M2Y="
-          title="Instagram"
-        >
-          <SvgInstagram/>
-        </SoMeButton>
-      </div>
-    </main>
+    <>
+      <HeroImage />
+      <main className="grid place-content-center bg-yellow-200 min-h-screen">
+        <div className="flex gap-3 mt-4 grid grid-flow-col grid-rows-6 md:grid-rows-1 lg:grid-rows-1">
+          <SoMeButton
+            href="https://sonofkong.bandcamp.com/"
+            title="BandCamp"
+          >
+            <SvgBandcamp/>
+          </SoMeButton>
+          <SoMeButton
+            href="https://open.spotify.com/artist/14tfZFUIaavTfIOyqBP9Mp"
+            title="Spotify"
+          >
+            <SvgSpotify/>
+          </SoMeButton>
+          <SoMeButton
+            href="https://www.youtube.com/channel/UCY5pevqMn98aQClj2TBWkoQ"
+            title="Youtube"
+          >
+            <SvgYoutube/>
+          </SoMeButton>
+          <SoMeButton
+            href="https://music.apple.com/artist/son-of-kong/1049109342"
+            title="Apple Music"
+          >
+            <SvgAppleMusic
+            />
+          </SoMeButton>
+          <SoMeButton
+            href="https://instagram.com/sonofkong_?igshid=YmMyMTA2M2Y="
+            title="Instagram"
+          >
+            <SvgInstagram/>
+          </SoMeButton>
+        </div>
+      </main>
+    </>
   )
 }
---
+++ src/components/HeroImage.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+export const HeroImage = () => {
+  return (
+    // <header className="flex bg-yellow-200 h-96">
+    //   <div className=" bg-no-repeat bg-cover bg-center bg-fixed"></div>
+    //   {/* background-image: url('./media/img/SOKlarge.png'); */}
+    // </header>
+    <header className="zoom out you fucking annoying bastard ">
+      <div className='bg-sokHero 
+                      bg-[length:800px_500px]
+                      bg-fixed
+                      bg-auto
+                      bg-no-repeat
+                      bg-fixed
+                      bg-cover
+                      bg-contain
+                      bg-center
+                      bg-yellow-200
+                      w-full
+                      h-96
+                      '>
+                      {/* // WHY DOES IT NEED A FIXED FUCKING HEIGHT FUCK YOU h-96 - what is with the amount og bg classes, so stupid! */}
+      </div>
+    </header>
+  )
+};
