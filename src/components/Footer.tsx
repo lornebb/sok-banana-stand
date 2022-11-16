799a3a60087b28d6942ee4ad538866f11b1af3b6
lornebb <51881266+lornebb@users.noreply.github.com>
2022-11-16 18:01:38 +0100

    moved soMe to footer, made footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,56 @@
+import React from "react"
+import { SvgAppleMusic } from "../icons/svg-appleMusic";
+import { SvgBandcamp } from "../icons/svg-bandcamp";
+import { SvgInstagram } from "../icons/svg-instagram";
+import { SvgSpotify } from "../icons/svg-spotify";
+import { SvgYoutube } from "../icons/svg-youtube";
+import { SoMeButton, SoMeList } from "./SoMeButton";
+
+const date = new Date();
+const yearToday = date.getFullYear();
+
+export const Footer = () => {
+  return (
+    <footer className="grid bg-yellow-200 text-center place-content-center">
+      <hr className="bg-[#1da1f2]"/>
+      <div className="flex pt-3 gap-3 grid grid-flow-col grid-rows-6 sm:grid-rows-1 sm:grid-cols-2 md:auto-cols-min ">
+        <SoMeButton
+          title={SoMeList.bandcamp.title}
+          href={SoMeList.bandcamp.href}
+        >
+          <SvgBandcamp/>
+        </SoMeButton>
+        <SoMeButton
+          title={SoMeList.spotify.title}
+          href={SoMeList.spotify.href}
+        >
+          <SvgSpotify/>
+        </SoMeButton>
+        <SoMeButton
+          title={SoMeList.youtube.title}
+          href={SoMeList.youtube.href}
+        >
+          <SvgYoutube/>
+        </SoMeButton>
+        <SoMeButton
+          title={SoMeList.appleMusic.title}
+          href={SoMeList.appleMusic.href}
+        >
+          <SvgAppleMusic
+          />
+        </SoMeButton>
+        <SoMeButton
+          title={SoMeList.instagram.title}
+          href={SoMeList.instagram.href}
+        >
+          <SvgInstagram/>
+        </SoMeButton>
+      </div>
+      <div className="text-gray-700 text-center p-4 italic">
+        <p>
+          ©{yearToday} <a className="" href="/">SoK</a>
+        </p>
+      </div>
+    </footer>
+  )
+};
--- src/components/SoMeButton.tsx
+++ src/components/SoMeButton.tsx
@@ -6,20 +6,35 @@ interface ISoMeButton {
   children: JSX.Element | JSX.Element[],
 }
 
-export const SoMeButton = ({href, title, children}: ISoMeButton) => {
+export interface ISoMeList {
+  bandcamp: {},
+  spotify: {},
+  appleMusic: {},
+  youtube: {},
+  instagram: {},
+}
 
+export const SoMeList:ISoMeList = {
+  bandcamp: { title: "BandCamp", href: "https://sonofkong.bandcamp.com/" },
+  spotify: { title: "Spotify", href: "https://open.spotify.com/artist/14tfZFUIaavTfIOyqBP9Mp" },
+  appleMusic: { title: "AppleMusic", href: "https://music.apple.com/artist/son-of-kong/1049109342" },
+  youtube: { title: "Youtube", href: "https://www.youtube.com/channel/UCY5pevqMn98aQClj2TBWkoQ" },
+  instagram: { title: "Instagram", href: "https://instagram.com/sonofkong_?igshid=YmMyMTA2M2Y=" },
+}
+
+export const SoMeButton = ({href, title, children}: ISoMeButton) => {
   return (
-  <a
-    href={href}
-    target="_blank"
-    rel="noopener noreferrer"
-    type="button"
-    className="text-yellow-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50"
-  >
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      type="button"
+      className="text-yellow-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50"
+    >
     {children}
-    <text>
+    <p>
       {title}
-    </text>
+    </p>
   </a>
   )
 };
