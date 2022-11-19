54ab16bf61d8d0a60d0edd72da4e1c535bd2ba94
Lorne Ashley <info@nullanion.com>
2022-11-19 13:05:15 +0100

    feat: added image, added hero component (#19)
    * added image, added hero component. unfinished
    
    * HeroImg as an img tag
    
    * Fixed hero image location on main page.
    
    Co-authored-by: Nick Dalby <nckdlby@gmail.com>

---
+++ src/components/HeroImg.tsx
@@ -0,0 +1,9 @@
+export const HeroImg = () => {
+  return (
+    <div className="bg-yellow-200">
+      <header className="flex max-w-lg justify-center mx-auto">
+        <img src="src/media/img/SOKlarge.png" alt="son of kong logo" />
+      </header>
+    </div>
+  )
+}
