75b93f8a23fe52368a9ead3953695cde113e4261
lornebb <51881266+lornebb@users.noreply.github.com>
2022-11-29 02:50:28 +0100

    added hero image from imageKit hosting

--- src/components/HeroImg.tsx
+++ src/components/HeroImg.tsx
@@ -2,7 +2,10 @@ export const HeroImg = () => {
   return (
     <div className="br-transparent">
       <header className="flex max-w-lg justify-center mx-auto">
-        <img src="src/media/img/SOKlarge.png" alt="son of kong logo" />
+        <img
+          src="https://ik.imagekit.io/y6pjjzivw/SoK-Banana-Stand/SOK_Banana_Logo_v8-06.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669685979893"
+          alt="son of kong logo"
+        />
       </header>
     </div>
   )
