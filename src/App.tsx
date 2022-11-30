63404b430885dcc99ca960a281e66252b18931b1
nick <nckdlby@gmail.com>
2022-11-30 10:59:31 +0100

    add wipe component + cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React from "react"
-import { FloatingBananas } from "./components/FloatingBananas"
-import { Footer } from "./components/Footer"
-import { HeroImg } from "./components/HeroImg"
+import { FloatingBananas } from './components/FloatingBananas'
+import { Footer } from './components/Footer'
+import { HeroImg } from './components/HeroImg'
+import Wipe from './components/Wipe'
 
 export const App = () => {
   return (
-    <>
-      <main className="grid place-content-center bg-transparent min-h-screen relative">
+    <div className="grid place-content-center bg-transparent min-h-screen relative">
       <HeroImg />
       <Footer />
       <div className="absolute w-full h-full top-0 -z-10">
-        <FloatingBananas count={100}/>
+        <FloatingBananas count={100} />
       </div>
-      </main>
-    </>
+      <Wipe />
+    </div>
   )
 }
