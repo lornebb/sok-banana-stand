02903efeadc2f6bfce6a01c33e49680d08dd0f32
nick <nckdlby@gmail.com>
2022-11-30 11:00:08 +0100

    wipe component

---
+++ src/components/Wipe.tsx
@@ -0,0 +1,9 @@
+const Wipe = () => {
+  return (
+    <div
+      className="wipe absolute w-screen h-screen bg-gray-900"
+      transition-style="out:circle:center"
+    ></div>
+  )
+}
+export default Wipe
