32bcdfdb8096cce5ad9b6478f32b10c8f4ac7f70
nick <nckdlby@gmail.com>
2022-12-28 12:07:31 +0100

    clean up, reduced padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { SvgAppleMusic } from '../icons/svg-appleMusic'
-import { SvgBandcamp } from '../icons/svg-bandcamp'
-import { SvgInstagram } from '../icons/svg-instagram'
-import { SvgSpotify } from '../icons/svg-spotify'
-import { SvgTidal } from '../icons/svg-tidal'
-import { SvgYoutube } from '../icons/svg-youtube'
 import { SoMeButton } from './SoMeButton'
 
-import { SiSpotify, SiApplemusic, SiBandcamp, SiTidal, SiYoutube, SiInstagram } from 'react-icons/si'
+import {
+  SiApplemusic,
+  SiBandcamp,
+  SiInstagram,
+  SiSpotify,
+  SiTidal,
+  SiYoutube,
+} from 'react-icons/si'
 
 const date = new Date()
 const yearToday = date.getFullYear()
 
 const SoMeList = [
-  { title: 'BandCamp', href: 'https://sonofkong.bandcamp.com/', icon: <SiBandcamp /> },
+  {
+    title: 'BandCamp',
+    href: 'https://sonofkong.bandcamp.com/',
+    icon: <SiBandcamp />,
+  },
   {
     title: 'Spotify',
     href: 'https://open.spotify.com/artist/14tfZFUIaavTfIOyqBP9Mp',
@@ -24,7 +28,11 @@ const SoMeList = [
     href: 'https://music.apple.com/artist/son-of-kong/1049109342',
     icon: <SiApplemusic />,
   },
-  { title: 'Tidal', href: 'https://tidal.com/browse/artist/8412421', icon: <SiTidal /> },
+  {
+    title: 'Tidal',
+    href: 'https://tidal.com/browse/artist/8412421',
+    icon: <SiTidal />,
+  },
   {
     title: 'Youtube',
     href: 'https://www.youtube.com/channel/UCY5pevqMn98aQClj2TBWkoQ',
@@ -43,7 +51,7 @@ const SoMeButtonMap = SoMeList.map((e, index) => {
 
 export const Footer = () => {
   return (
-    <footer className="grid place-content-center absolute bottom-0 left-1/2 -translate-x-1/2">
+    <footer className="grid place-content-center absolute bottom-0 left-1/2 -translate-x-1/2 ">
       <div
         className=" 
         flex flex-row space-x-3 
