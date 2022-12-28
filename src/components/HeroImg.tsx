d546cb2f0da27cab4f245febd3b708f94a8490ae
nick <nckdlby@gmail.com>
2022-12-28 12:07:53 +0100

    margin and padding

--- src/components/HeroImg.tsx
+++ src/components/HeroImg.tsx
@@ -1,7 +1,7 @@
 export const HeroImg = () => {
   return (
     <div className="br-transparent">
-      <header className="flex max-w-lg justify-center mx-auto">
+      <header className="flex max-w-lg justify-center mx-auto m-10 px-2">
         <img
           src="https://ik.imagekit.io/y6pjjzivw/SoK-Banana-Stand/SOK_Banana_Logo_v8-06.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669685979893"
           alt="son of kong logo"
