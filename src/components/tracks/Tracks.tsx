30ebd3fcc36694a388fae6fd204790d8c54df720
nick <nckdlby@gmail.com>
2022-12-29 11:40:14 +0100

    move out tracks data

--- src/components/tracks/Tracks.tsx
+++ src/components/tracks/Tracks.tsx
@@ -1,19 +1,11 @@
 import Track from './Track'
 
-export const Tracks = () => {
+export const Tracks = ({tracks}) => {
   // 170 to 700 range for the size
-
-  const tracks = [
-    { title: 'Never Ever Land', id: '614336722' },
-    { title: 'Get Up', id: '4132457639' },
-    { title: 'Club Tropigwana', id: '743691718' },
-    { title: 'Remember Me', id: '3008176448' },
-    { title: 'Never Ever Land', id: '3974639248' },
-  ]
 
   return (
     <div className=" my-0 mx-0 overflow-hidden" >
-      <div className="flex gap-4 overflow-scroll py-6 px-36">
+      <div className="flex gap-4 overflow-scroll py-6 px-6 sm:px-36 hide-scrollbar">
         {tracks.map((track) => (
           <Track size={300} trackId={track.id} key={track.id}/>
         ))}
