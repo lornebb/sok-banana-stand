07d1e9393734b73e07364d0655c233678ebc4dfa
nick <nckdlby@gmail.com>
2022-12-29 11:39:59 +0100

    sizing and scrolling

--- src/components/HeroImg.tsx
+++ src/components/HeroImg.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react'
+
 export const HeroImg = () => {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.pageYOffset >= 200)
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <div className="br-transparent">
-      <header className="flex max-w-lg justify-center mx-auto m-6 px-2">
+    <div className={`flex h-screen justify-center mx-auto items-center`}>
+      <div
+        className={`fixed max-w-xl px-4 ${scrolled ? 'transition scale-50 -top-16 mt-6 sm:mt-0' : ''}`}
+      >
         <img
-          src="https://ik.imagekit.io/y6pjjzivw/SoK-Banana-Stand/SOK_Banana_Logo_v8-06.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669685979893"
+          className="w-full"
+          src="https://ik.imagekit.io/y6pjjzivw/SoK-Banana-Stand/SOK_Banana_Logo_v8-06.png"
           alt="son of kong logo"
         />
-      </header>
+      </div>
     </div>
   )
 }
