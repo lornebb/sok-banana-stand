cf19f58e05b07a245da4a372a08fb2cd62d24164
Nick <nick.dalby@diva-e.com>
2023-03-08 17:09:53 +0100

    absolutely positioned the text to line up

--- src/components/tracks/Tracks.tsx
+++ src/components/tracks/Tracks.tsx
@@ -11,10 +11,10 @@ export const Tracks = ({
   // 170 to 700 range for the size
 
   return (
-    <div className='m-4 justify-center'>
-      <div className='font-joeseph opacity-60 text-4xl justify-end'>{year}</div>
+    <div className='m-4 justify-center relative'>
       <div className='my-0 mx-0 overflow-hidden flex justify-center'>
-        <div className='flex gap-4 overflow-scroll py-6 px-6 sm:px-36 hide-scrollbar'>
+        <div className='flex gap-4 overflow-scroll pt-10 pb-6 px-6 sm:px-36 hide-scrollbar'>
+          <div className='font-joeseph opacity-60 text-4xl absolute -top-2'>{year}</div>
           {tracks.map((track) => (
             <Track size={300} trackId={track.id} key={track.id} />
           ))}
